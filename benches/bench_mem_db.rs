d6e1141e0b2b494e91bb23b8efbb76ba2e06e2ee
jiangfb <1209467293@qq.com>
2020-07-03 10:01:54 +0800

    add iter test

--- benches/bench_mem_db.rs
+++ benches/bench_mem_db.rs
@@ -4,6 +4,9 @@ extern crate test;
 use test::Bencher;
 
 use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::RwLock;
 
 use atom::Atom;
 use bon::WriteBuffer;
@@ -11,9 +14,11 @@ use guid::GuidGen;
 use pi_db::db::{TabKV, TabMeta};
 use pi_db::mgr::{DatabaseWare, Mgr};
 use r#async::rt::multi_thread::{MultiTaskPool, MultiTaskRuntime};
+use r#async::rt::single_thread::{SingleTaskRunner};
 use r#async::rt::AsyncRuntime;
 use pi_db::memery_db::MemDB;
 use sinfo;
+
 
 #[bench]
 fn bench_mem_db_write_single_tab(b: &mut Bencher) {
@@ -61,6 +66,91 @@ fn bench_mem_db_concurrent_read(b: &mut Bencher) {
 			test_mem_db_concurrent_read(rt_clone).await
 		});
 	});
+}
+
+#[bench]
+fn bench_mem_db_iter(b: &mut Bencher) {
+	let pool = MultiTaskPool::new("Store-Runtime".to_string(), 8, 1024 * 1024, 10, Some(10));
+	let rtm: MultiTaskRuntime<()> = pool.startup(false);
+
+	let runner  = SingleTaskRunner::new();
+	let rt = runner.startup().unwrap();
+
+	let flag = Arc::new(AtomicBool::new(false));
+	let flag_clone = flag.clone();
+	let mgr11 = Arc::new(RwLock::new(None));
+	let mgr11_clone = mgr11.clone();
+
+	let _ = rtm.spawn(rtm.alloc(), async move {
+		let mgr = setup_data().await;
+		*mgr11_clone.write().unwrap() = Some(mgr);
+		flag_clone.store(true, Ordering::Relaxed);
+	});
+
+	loop {
+		if flag.load(Ordering::Relaxed) {
+			break;
+		}
+	}
+
+	let mgr = mgr11.write().unwrap().take().unwrap();
+
+	b.iter(|| {
+		let mgr_clone = mgr.clone();
+		let _ = rt.spawn(rt.alloc(), async move {
+			test_mem_db_iter(mgr_clone).await;
+		});
+		runner.run_once();
+	});
+}
+
+async fn setup_data() -> Mgr {
+	let mgr = Mgr::new(GuidGen::new(0, 0));
+	let ware = DatabaseWare::new_mem_ware(MemDB::new());
+	let _ = mgr.register(Atom::from("memory"), Arc::new(ware)).await;
+	let mut tr = mgr.transaction(true).await;
+	let meta = TabMeta::new(sinfo::EnumType::Str, sinfo::EnumType::Str);
+	tr.alter(&Atom::from("memory"), &Atom::from("hello"), Some(Arc::new(meta))).await;
+
+	tr.prepare().await;
+	tr.commit().await;
+
+	let mut tr2 = mgr.transaction(true).await;
+	
+	let mut items = vec![];
+
+	for i in 0..20000 {
+		let key = format!("hello world {:?}", i);
+		let mut wb = WriteBuffer::new();
+		wb.write_bin(key.as_bytes(), 0..key.len());
+
+		items.push(TabKV {
+			ware: Atom::from("memory"),
+			tab: Atom::from("hello"),
+			key: Arc::new(wb.bytes.clone()),
+			value: Some(Arc::new(wb.bytes)),
+			index: 0,
+		});
+	}
+
+	tr2.modify(items.clone(), None, false).await;
+	tr2.prepare().await;
+	tr2.commit().await;
+
+	mgr
+}
+
+async fn test_mem_db_iter(mgr: Mgr) {
+	let mut tr3 = mgr.transaction(false).await;
+	
+	let mut iter = tr3.iter(&Atom::from("memory"), &Atom::from("hello"), None, false, None).await.unwrap();
+
+	while let Some(Ok(Some(elem))) = iter.next() {
+		// println!("elem = {:?}", elem);
+	}
+
+	tr3.prepare().await;
+	tr3.commit().await;
 }
 
 async fn test_mem_db_concurrent_read(rt: MultiTaskRuntime<()>) {
--- tests/test_memdb.rs
+++ tests/test_memdb.rs
@@ -10,6 +10,63 @@ use pi_db::db::{TabKV, TabMeta};
 use bon::WriteBuffer;
 
 #[test]
+fn test_mem_db_iter() {
+	let pool = MultiTaskPool::new("Store-Runtime".to_string(), 4, 1024 * 1024, 10, Some(10));
+	let rt: MultiTaskRuntime<()>  = pool.startup(true);
+	let mgr = Mgr::new(GuidGen::new(0, 0));
+	let ware = DatabaseWare::new_mem_ware(MemDB::new());
+
+	let _ = rt.spawn(rt.alloc(), async move {
+		let _ = mgr.register(Atom::from("memory"), Arc::new(ware)).await;
+		let mut tr = mgr.transaction(true).await;
+		let meta = TabMeta::new(sinfo::EnumType::Str, sinfo::EnumType::Str);
+		tr.alter(&Atom::from("memory"), &Atom::from("hello"), Some(Arc::new(meta))).await;
+
+		tr.prepare().await;
+		tr.commit().await;
+
+		let mut tr5 = mgr.transaction(true).await;
+		let tab_info= mgr.tab_info(&Atom::from("memory"), &Atom::from("hello")).await;
+
+		let mgr2 = mgr.clone();
+		let mut tr2 = mgr.transaction(true).await;
+		
+		let mut items = vec![];
+
+		for i in 0..20 {
+			let key = format!("hello world {:?}", i);
+			let mut wb = WriteBuffer::new();
+			wb.write_bin(key.as_bytes(), 0..key.len());
+
+			items.push(TabKV {
+				ware: Atom::from("memory"),
+				tab: Atom::from("hello"),
+				key: Arc::new(wb.bytes.clone()),
+				value: Some(Arc::new(wb.bytes)),
+				index: 0,
+			});
+		}
+
+		tr2.modify(items.clone(), None, false).await;
+		tr2.prepare().await;
+		tr2.commit().await;
+
+		let mut tr3 = mgr.transaction(false).await;
+		let size = tr3.tab_size(&Atom::from("memory"), &Atom::from("hello")).await;
+		println!("size = {:?}", size);
+		let mut iter = tr3.iter(&Atom::from("memory"), &Atom::from("hello"), None, false, None).await.unwrap();
+
+		while let Some(Ok(Some(elem))) = iter.next() {
+			println!("elem = {:?}", elem);
+		}
+		
+		tr3.prepare().await;
+		tr3.commit().await;
+	});
+	std::thread::sleep(std::time::Duration::from_secs(20));
+}
+
+#[test]
 fn test_memory_db() {
 	let pool = MultiTaskPool::new("Store-Runtime".to_string(), 4, 1024 * 1024, 10, Some(10));
 	let rt: MultiTaskRuntime<()>  = pool.startup(true);
